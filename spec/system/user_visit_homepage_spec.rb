90cbca1686eec029dbc020d251160df13ec4f4e3
Vinicius Santos <vinicius.vph@gmail.com>
2021-07-12 15:36:22 -0300

    user can access homepage now

---
+++ spec/system/user_visit_homepage_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'User visit homepage' do
+    it "- Should be able access homepage" do
+        visit root_path
+
+        expect(current_path).to eq(root_path)
+        expect(page).to have_text("Analisador de Transações Financeiras")
+        expect(page).to have_text("Faça upload do arquivo para análise")
+        expect(page).to have_css('input#tab_input')    
+        expect(page).to have_button("Enviar")    
+    end
+    
+end
