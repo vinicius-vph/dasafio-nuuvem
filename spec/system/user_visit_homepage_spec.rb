af7c1457fe1c131b088195130a428112c5ae4419
Vinicius Santos <vinicius.vph@gmail.com>
2021-07-15 13:04:23 -0300

    add tests to project

--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe 'User visit homepage' do
-    it "- Should be able access homepage" do
-        visit root_path
+  it '- Should be able access homepage' do
+    visit root_path
 
-        expect(current_path).to eq(root_path)
-        expect(page).to have_text("Analisador de Transações Financeiras")
-        expect(page).to have_text("Faça upload do arquivo para análise")
-        expect(page).to have_css('input#tab_input')    
-        expect(page).to have_button("Enviar")    
-    end
-    
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('Analisador de Transações Financeiras')
+    expect(page).to have_text('Faça upload do arquivo para análise')
+    expect(page).to have_css('input#tab_input')
+    expect(page).to have_button('Enviar')
+  end
 end
