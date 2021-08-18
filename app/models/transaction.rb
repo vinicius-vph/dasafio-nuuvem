17668877a6ca8d5af7e054ba157c75948bb413c6
Vinicius Santos <vinicius.vph@gmail.com>
2021-08-18 18:46:38 -0300

    add new scopes  on transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,10 +4,12 @@ class Transaction < ApplicationRecord
   belongs_to :item
 
   scope :total_transactions, lambda {
-                               select(:merchant, :item_price, :purchase_count)
+                               select(:merchant, :item_price, :purchase_count, :processed_at)
                                  .group(:merchant)
                                  .total_cost
                              }
+
+  scope :last_uploaded_file, -> { where(processed_at: present? ? last.processed_at : Date.current) }
 
   def self.total_cost
     sum('item_price * purchase_count')
