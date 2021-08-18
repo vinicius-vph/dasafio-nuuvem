be695629134be9016683c3de8e150fe270431acc
Vinicius Santos <vinicius.vph@gmail.com>
2021-08-18 18:45:44 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  post 'transaction/', to: 'transaction#create'
-  get 'transaction/results', to: 'transaction#results'
+  resources :transaction, only: %i[index new create]
 end
